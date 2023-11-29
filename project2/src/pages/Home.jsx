e6a536b99a0cb7a055c78461d1a8ce38755616c5
letishabitu <lkidanu@gmail.com>
2023-11-29 13:58:37 -0500

    5th commit

--- project2/src/pages/Home.jsx
+++ project2/src/pages/Home.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>This is Home Page</div>
+    <div>
+      <p>This is Home page. This application displays New York Times articles  using different key words. For example if you want to 
+        search an article about sport, follow these steps. 
+        1. click the News tab, 
+        2. It will open a window and search the required article by typing the search key word.
+        3.If you didn't put any keyword, the system will display recent articles by default.  
+      </p>
+    </div>
   )
 }
 
