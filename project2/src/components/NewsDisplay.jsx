f3cffe984dee8f52fd21a9aa2cc16f9864177119
letishabitu <lkidanu@gmail.com>
2023-12-01 14:26:37 -0500

    9th commit

--- project2/src/components/NewsDisplay.jsx
+++ project2/src/components/NewsDisplay.jsx
@@ -13,7 +13,7 @@ function NewsDisplay({ news }) {
              return(
                 <>
                 <h3>{abstract}</h3>
-                <h3>{web_url}</h3> 
+                
                 <a href={web_url}> click link here</a>
                 </>
                 
